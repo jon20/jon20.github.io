d78b653fd030e76d9d6ffe90a3780eecb4369ad0
jon <kawadu1221@gmail.com>
2018-07-27 03:34:35 +0900

    add header

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+   <nav class="nav-extended">
+    <div class="nav-wrapper grey darken-3">
+      <a href="#" class="brand-logo center">Welcome My HomePage!!</a>
+    </div>
+    <div class="nav-content grey darken-3">
+      <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="#test1">Test 1</a></li>
+        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
+        <li class="tab"><a href="#test4">Test 4</a></li>
+      </ul>
+    </div>
+  </nav>
+         
+</template>
