53c673a7373d9413c46e902518077487bc249900
jon <kawadu1221@gmail.com>
2018-08-10 22:26:47 +0900

    add twitter tileline

--- components/Header.vue
+++ components/Header.vue
@@ -3,13 +3,6 @@
     <div class="nav-wrapper grey darken-3">
       <a href="#" class="brand-logo center">Welcome My HomePage!!</a>
     </div>
-    <div class="nav-content grey darken-3">
-      <ul class="tabs tabs-transparent">
-        <li class="tab"><a href="#test1">Test 1</a></li>
-        <li class="tab"><a class="active" href="#test2">Test 2</a></li>
-        <li class="tab"><a href="#test4">Test 4</a></li>
-      </ul>
-    </div>
   </nav>
          
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,31 @@
 <template>
 <div class = 'main'>
+    <div class="row">
+    <div class="col s12 m12">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">About me !!</span>
+          <p>I'm a college student. this page is portal site !!!</p>
+        </div>
+      </div>
+    <div class="col s12 m4">
+      <div class="card ">
+        <div class="card-content black-text">
+          <span class="card-title">Twitter</span>
+        </div>
+          <a class="twitter-timeline" data-lang="ja" data-height="900"  data-link-color="#F5F8FA" href="https://twitter.com/jon_ground?ref_src=twsrc%5Etfw">Tweets by jon_ground</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      </div>
+    </div>
+    </div>
+  </div>
+  <!--
   <div class="section white">
     <img src="~/assets/flog.jpg">
   </div>
-      
   <parallax fixed>
     <img src="~/assets/back.jpg" alt="very cool bg">
   </parallax>
+-->
 </div>
 </template>
 
