80cc0ad5bf3806f3850df0bd0bc0928db5d244dd
jon <kawadu1221@gmail.com>
2019-01-01 16:24:34 +0900

    sample template

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,45 @@
-module Main exposing (..)
-import Bulma.CDN exposing (stylesheet)
+module App exposing (..)
 
-view : Model -> Html msg
-view model
-  = div []
-    [ stylesheet
-    , text "wow!"
-    ]
+import Browser
+import Html exposing (Html, input, div, text)
+import Html.Events exposing (onInput)
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+-- MODEL
+
+type alias Model =
+  {
+    value: String
+  }
+
+init : Model
+init =
+  {
+    value = "type something below"
+  }
+
+
+-- UPDATE
+
+type Msg = 
+  UpdateModel String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    UpdateModel value ->
+      ({ model | value = value })
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [] [
+    div [] [text model.value],
+    input [onInput UpdateModel] []
+  ]
